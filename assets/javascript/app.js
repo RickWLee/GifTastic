0690d6babbd74cb9f7a8a921cb9dac0e6ea2626e
Weihao Rick Lee <kenchi133@hotmail.com>
2016-09-08 18:30:51 -0500

    create button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,48 @@
 $(document).ready(function() { 
 
+	// // 
+	// var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
 
-	var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-
-	$.ajax({url: queryURL, method: 'GET'})
-	 .done(function(response) {
-	    console.log(response);
-	}); 
+	// $.ajax({url: queryURL, method: 'GET'})
+	//  .done(function(response) {
+	//     // console.log(response);
+	// }); 
 
   // Initial array of movies
-	var movies = ['The Matrix', 'The Notebook', 'Mr. Nobody', 'The Lion King'];
+	var animals= ['bird', 'dog', 'cat', 'pokemon', 'naruto'];
 
 	// ========================================================
 
 	// Generic function for capturing the movie name from the data-attribute
-	function alertMovieName(){
-		var movieName = $(this).data("name");
+	// function alertName(){
+	// 	var searchName = $(this).data("name");
 
-		alert(movieName);
-	}
+	// 	alert(searchName);
+	// }
 
 	// ========================================================
 
 	// Generic function for displaying movie data 
-	function renderButtons(){ 
+	function createButtons(){ 
 
 		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
-		$('#moviesView').empty();
+		// $('#animalsButtons').empty();
 
 		// Loops through the array of movies
-		for (var i = 0; i < movies.length; i++){
+		for (var i = 0; i < animals.length; i++){
 
 			// Then dynamicaly generates buttons for each movie in the array
 
 			// Note the jQUery syntax here... 
-		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    a.addClass('movie'); // Added a class 
-		    a.attr('data-name', movies[i]); // Added a data-attribute
-		    a.text(movies[i]); // Provided the initial button text
-		    $('#moviesView').append(a); // Added the button to the HTML
-		}
+		    var a = $('<button>'); // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+		    // a.addClass('animals'); // Added a class 
+		    a.attr('data-name', animals[i]); // Added a data-attribute
+		    a.text(animals[i]); // Provided the initial button text
+		    $('#animalsButtons').append(a); // Added the button to the HTML
+		};
 	}
 
+	createButtons();
 
 
 });
