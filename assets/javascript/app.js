843f6c9b6cac8fdfe274ee3eb8cdf5796518f0db
Weihao Rick Lee <kenchi133@hotmail.com>
2016-09-10 00:01:16 -0500

    fix the pausing animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,9 @@
 $(document).ready(function() { 
 
-	// // 
-	
+//set array for animal
+	var topics= ['bird', 'dog', 'cat', 'monkey', 'fox', 'lion','tiger'];
 
-
-  // Initial array of movies
-	var topics= ['bird', 'dog', 'cat', 'monkey', 'fox'];
-
-	
-
-	// Create Animal button on the html
-	// function createButtons(){ 
-
-		//clear up Div for id = animalsButtons
-		
-
-		// Loops through the array of array animal
+	// Loops through the array of array animal
 		for (var i = 0; i < topics.length; i++){
 			// Then dynamicaly generates buttons for each movie in the array
 			// Note the jQUery syntax here... 
@@ -25,77 +13,75 @@ $(document).ready(function() {
 		    a.text(topics[i]); // Provided the initial button text
 		    $('#animalsButtons').append(a); // Added the button to the HTML
 		};
-	// }
+	
+	//not understanding about Buttons function at all... all this is pretty much cut paste and written by someone else.
+	//no idea about function at all.
 
-	// createButtons();
+	$(document).on("click", '.livething', function(){
 
-	// function getInfo(){
-$('.livething').on("click", function(){
+		$('#animals').empty();
 
-	$('#animals').empty();
+		var searchAnimal=$(this).data("name");
 
-	var searchAnimal=$(this).data("name");
-
-
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchAnimal+"&api_key=dc6zaTOxFJmzC&limit=10";
-		
-		//search under ajax
-		$.ajax({
-			url: queryURL, method: 'GET'
-			})
-			.done(function(response) {
-				console.log(response);
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchAnimal+"&api_key=dc6zaTOxFJmzC&limit=10";
 			
+			//search under ajax
+			$.ajax({
+				url: queryURL, method: 'GET'
+				})
+				.done(function(response) {
+					console.log(response);
+				
 
 
-	//Output for the response
+		//Output for the response.  basically copy from other homework. 
 
-	        var results = response.data;
+		        var results = response.data;
 
-	        for (var i=0; i < results.length; i++) {
+		        for (var i=0; i < results.length; i++) {
 
 
-	                var gifDiv = $('<div>');
+		                var gifDiv = $('<div>');
 
-	                var rating = results[i].rating;
+		                var rating = results[i].rating;
 
-	                var p = $('<p>').text( "Rating: " + rating);
- 
-	                
-	                var personImage = $('<img>');
-	                	personImage.attr('src', results[i].images.fixed_height_still.url);
+		                var p = $('<p>').text( "Rating: " + rating);
+	 
+		                
+		                var personImage = $('<img>');
+		                	personImage.attr('src', results[i].images.fixed_height_still.url);
 
-	                	//adding pausing attribute for the gif
-	                	personImage.attr('data-still', results[i].images.fixed_height_still.url);
-	                	personImage.attr('data-animate', results[i].images.fixed_height.url);
-	                	personImage.attr('data-state','still')
-	                	personImage.attr('class','gif')
+		                	//adding pausing attribute for the gif (This I have figure out and put the following)
+		                	personImage.attr('data-still', results[i].images.fixed_height_still.url);
+		                	personImage.attr('data-animate', results[i].images.fixed_height.url);
+		                	personImage.attr('data-state','still')
+		                	personImage.attr('class','gif')
+		            
+						gifDiv.append(personImage);
+		                gifDiv.append(p);
+		                
+		             
+
+		        		$('#animals').prepend(gifDiv);
+
+	         	}
+	         	//make the gif pausing or animate -- this copy from other section of homework
+
+	         	$('.gif').on('click', function(){
+
+		            var state = $(this).attr('data-state');
+		    
+		            if ( state == 'still'){
+		                $(this).attr('src', $(this).data('animate'));
+		                $(this).attr('data-state', 'animate');
+		            }else{
+		                $(this).attr('src', $(this).data('still'));
+		                $(this).attr('data-state', 'still');
+		            }
 	            
+	        	});
 
-	                gifDiv.append(p);
-	                gifDiv.append(personImage);
-	             
-
-	                $('#animals').prepend(gifDiv);
-
-	                //make the gif pausing or anime
-
-	          		$('.gif').on('click', function(){
-
-            			var state = $(this).attr('data-state');
-    
-            				if ( state == 'still'){
-                				$(this).attr('src', $(this).data('animate'));
-                				$(this).attr('data-state', 'animate');
-            				}else{
-                				$(this).attr('src', $(this).data('still'));
-                				$(this).attr('data-state', 'still');
-            				}
-            
-        			});
-         	}
-
-		}); 
+			}); 
 
 	});
 
