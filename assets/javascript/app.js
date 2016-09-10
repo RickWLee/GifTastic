c55a3637278294ff865a40b07010eccb62c8953a
Weihao Rick Lee <kenchi133@hotmail.com>
2016-09-09 20:50:30 -0500

    push picture into result div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,80 @@
 $(document).ready(function() { 
 
 	// // 
-	// var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+	
 
-	// $.ajax({url: queryURL, method: 'GET'})
-	//  .done(function(response) {
-	//     // console.log(response);
-	// }); 
 
   // Initial array of movies
-	var animals= ['bird', 'dog', 'cat', 'pokemon', 'naruto'];
+	var animals= ['bird', 'dog', 'cat', 'monkey', 'fox'];
 
-	// ========================================================
+	
 
-	// Generic function for capturing the movie name from the data-attribute
-	// function alertName(){
-	// 	var searchName = $(this).data("name");
+	// Create Animal button on the html
+	// function createButtons(){ 
 
-	// 	alert(searchName);
-	// }
+		//clear up Div for id = animalsButtons
+		
 
-	// ========================================================
-
-	// Generic function for displaying movie data 
-	function createButtons(){ 
-
-		// Deletes the movies prior to adding new movies (this is necessary otherwise you will have repeat buttons)
-		// $('#animalsButtons').empty();
-
-		// Loops through the array of movies
+		// Loops through the array of array animal
 		for (var i = 0; i < animals.length; i++){
-
 			// Then dynamicaly generates buttons for each movie in the array
-
 			// Note the jQUery syntax here... 
 		    var a = $('<button>'); // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-		    // a.addClass('animals'); // Added a class 
+		    a.addClass('livething'); // Added a class 
 		    a.attr('data-name', animals[i]); // Added a data-attribute
 		    a.text(animals[i]); // Provided the initial button text
 		    $('#animalsButtons').append(a); // Added the button to the HTML
 		};
-	}
+	// }
 
-	createButtons();
+	// createButtons();
 
+	// function getInfo(){
+$('.livething').on("click", function(){
+
+	$('#animals').empty();
+
+	var searchAnimal=$(this).data("name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchAnimal+"&api_key=dc6zaTOxFJmzC&limit=10";
+		
+		
+		$.ajax({
+			url: queryURL, method: 'GET'
+			})
+			.done(function(response) {
+				console.log(response);
+			
+
+
+	//Output for the response
+
+	        var results = response.data;
+
+	        for (var i=0; i < results.length; i++) {
+
+
+	                var gifDiv = $('<div>');
+
+	                var rating = results[i].rating;
+
+	                var p = $('<p>').text( "Rating: " + rating);
+
+	                var personImage = $('<img>');
+	                personImage.attr('src', results[i].images.fixed_height.url);
+
+	                gifDiv.append(p);
+	                gifDiv.append(personImage);
+
+	                $('#animals').prepend(gifDiv);
+	   
+         	}
+		}); 
+
+	});
 
 });
+
+
+
+
+
